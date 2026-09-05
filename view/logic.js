e45642f4c806fca438917429ecaf0c9858dbeaa5
youssef <youssefabdelmoneim2@gmail.com>
2026-09-05 13:15:16 +0300

    feat: organize the code and remove usless exports

--- view/logic.js
+++ view/logic.js
@@ -1,8 +1,8 @@
-export function createListOfSongs(type, home, data, val) {
+export function createListOfSongs(type, home, data, ...val) {
   const list = data.map((song) => createSong(type, song)).join(" ");
 
   if (type === `home`) {
-    const query = val;
+    const [query] = val;
     const html = `
          <p class="titleOfList"> Top Songs ${query === `song` ? `` : ` In ${query} `} </p>
          <div class="topHits">
@@ -12,13 +12,14 @@ export function createListOfSongs(type, home, data, val) {
          `;
     home.insertAdjacentHTML("beforeend", html);
   } else if (type === `search`) {
-    const searchList = val;
+    const [searchList, player] = val;
     searchList.innerHTML = list;
     searchList.classList.remove("hidden");
     home.classList.add("hidden");
+    player.classList.add("hidden");
   }
 }
-export function createSong(type, song) {
+function createSong(type, song) {
   return `
   <div class="song${type === "home" ? "" : "_searchList"}" id="${song.trackId}">
   <img 
@@ -37,14 +38,21 @@ export function createSong(type, song) {
 </div>
       `;
 }
-export function setAudioPage(
+
+export function activateAudioPage(
   home,
   searchList,
   player,
   songs,
   songsSearchList,
-  e,
 ) {
+  document.addEventListener(
+    "click",
+    setAudioPage.bind(null, home, searchList, player, songs, songsSearchList),
+  );
+}
+
+function setAudioPage(home, searchList, player, songs, songsSearchList, e) {
   let song = e.target.closest(`.song`);
   if (!song) song = e.target.closest(`.song_searchList`);
   if (song) {
@@ -63,7 +71,7 @@ export function setAudioPage(
   </div>
 
   <div class="player-controller">
-    <audio controls>
+    <audio controls autoplay>
       <source src="${songData.previewUrl}" type="audio/mp4" />
       Your browser does not support the audio element.
     </audio>
@@ -77,20 +85,6 @@ export function setAudioPage(
     player.classList.remove("hidden");
   }
 }
-
-export function activateAudioPage(
-  home,
-  searchList,
-  player,
-  songs,
-  songsSearchList,
-) {
-  document.addEventListener(
-    "click",
-    setAudioPage.bind(null, home, searchList, player, songs, songsSearchList),
-  );
-}
-
 export function activateSearch(
   home,
   searchBar,
